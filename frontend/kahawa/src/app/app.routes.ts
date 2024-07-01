09a4569b664e522b54f8dab69a796836859073c1
stan-005 <agotistanley005@gmail.com>
2024-07-01 17:45:51 +0300

    userdashboard added

--- frontend/kahawa/src/app/app.routes.ts
+++ frontend/kahawa/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
+import { MenuComponent } from './components/menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path:'admin',component:AdminDashboardComponent},
+  {path:'user',component:UserDashboardComponent,children:[
+    {path:'menu',component:MenuComponent}
+  ]},
+
+];
+  

8b531f305f78baa3865ff22b23f82414ef895bca
Carolyne1063 <carolmartoz763@gmail.com>
2024-07-02 19:27:48 -0700

    added redirection to respective dashboards

--- frontend/kahawa/src/app/app.routes.ts
+++ frontend/kahawa/src/app/app.routes.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, provideRouter } from '@angular/router';
 
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
-// import { LoginComponent } from './components/login/login.component';
-// import { RegisterComponent } from './components/register/register.component';
 import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
@@ -17,7 +15,6 @@ import { FormsComponent } from './components/forms/forms.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
-//   { path: 'login', component: LoginComponent },
   { path: 'forms', component: FormsComponent },
   { path: 'admin', component:AdminComponent, children:[
     {path: 'menu', component: MenuComponent},
